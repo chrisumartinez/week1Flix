d041460e2fcf46ef61b1b60871e8039858cc76f5
Chris Martinez <chrismartinez@Chriss-MacBook-Pro.local>
2018-09-18 19:19:05 -0700

    added collection view controller and collectionview

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Chris Martinez on 9/18/18.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
